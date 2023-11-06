07461f7803a3c573ac8e1a616f68a7686993ba3c
Hung Pham <pthungblaklx@gmail.com>
2023-11-07 04:43:02 +0700

    đăng ký đăng nhập được nhưng chưa logout được

--- lib/ui/screens.dart
+++ lib/ui/screens.dart
@@ -6,5 +6,4 @@ export '../ui/authentication/profile_screen.dart';
 export '../ui/novels/detail_novel_screen.dart';
 export '../ui/authentication/auth_manager.dart';
 export '../ui/authentication/auth_screen.dart';
-export './loading_sreen.dart';
 export 'home.dart';
