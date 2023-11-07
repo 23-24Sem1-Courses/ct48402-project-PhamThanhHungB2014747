c91114ad8d97f192faf85a658baf51383fefc51d
Hung Pham <pthungblaklx@gmail.com>
2023-11-08 00:26:53 +0700

    trang quản lý thêm sửa xóa sản phẩm nhưng chưa thực hiện được chứng thực realtime

--- lib/ui/novels/list_novel_screen.dart
+++ lib/ui/novels/list_novel_screen.dart
@@ -1,5 +1,5 @@
 import 'package:provider/provider.dart';
-import '../screens.dart';
+// import '../screens.dart';
 import './novel_creen.dart';
 import 'package:flutter/material.dart';
 import './novel_manager.dart';
